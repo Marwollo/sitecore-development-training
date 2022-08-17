ae0afd6323fc14c9511f1d60e690ffa591100227
Vuk Markovic <markovich.wolf@gmail.com>
2022-08-17 12:38:29 +0000

    Module 1 and Module 2

---
+++ events.tac.local.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace events.tac.local.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            Assert.True(true);
+        }
+    }
+}
